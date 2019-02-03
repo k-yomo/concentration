374f4eaf58449a03845daaea758b8bdc3763554c
yomoda07 <kanji.yy@gmail.com>
2019-02-04 05:50:38 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Kanji Yomoda on 2019/02/04.
+//  Copyright © 2019 Kanji Yomoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
